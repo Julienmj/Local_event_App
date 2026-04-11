116e2013129064e8a5788b37a8bda19601a68b83
0122345 <ntwarifiacre043@gmail.com>
2026-04-11 20:11:24 +0200

    ADD:Documentating on current pace/milestone, dbset to category & Event

--- backend/LocalEventOrganizer/LocalEventOrganizer/Data/ApiContext.cs
+++ backend/LocalEventOrganizer/LocalEventOrganizer/Data/ApiContext.cs
@@ -8,5 +8,7 @@ namespace LocalEventOrganizer.Data
         public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Event> Events { get; set; }
     }
 }
